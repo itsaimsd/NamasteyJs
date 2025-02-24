1687cc444d106ded8308b8f265a3baa5952ac91c
itsaimsd <madhusudantiwari2003@gmail.com>
2025-02-24 19:23:20 +0530

    all methods of js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Revision 
+// Revision
+
+const button = document.getElementById("myButton");
+button.addEventListener("click", function () {
+  console.log("Button is clicked");
+});
+
+//
+
+console.log("Event delegation and call back");
+
+const container = document.getElementById("container")
+
+ 
